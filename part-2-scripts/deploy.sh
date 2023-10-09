362ab8009b573f565b0e0fbca42bccbaf4d75a0c
Piyush Dubey <piyush.dubey@hp.com>
2023-10-09 16:51:34 +0530

    added the actions

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
 
-cd Week-5-with-axios-and-backend
+cd server
  git pull origin master
  cd server
+ npm i -g pm2
  pm2 kill
  pm2 start index.js
--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,4 @@
 ssh -t -i "cert-file.cer" ubuntu@ec2-url  "sudo bash ~/deploy.sh"
+
+
+ssh - t -i "todoapp.pem" ubuntu@ec2-3-126-92-65.eu-central-1.compute.amazonaws.com "sudo bash ~/deploy.sh"
